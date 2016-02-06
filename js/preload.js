81ba58450dbd50fba8ab764c1e303fe575543b7a
灵长目 <lizheminglzm@gmail.com>
2016-02-07 00:37:45 +0800

    ban某些浏览器

--- js/preload.js
+++ js/preload.js
@@ -22,14 +22,10 @@ else {
 	var isOnline = false;
 }
 
-// IE在页首显示警示
-var ua = navigator.userAgent.toLowerCase();
-if ((ua.match(/msie/i)) && (ua.match(/windows nt/i))){
-	document.write("<div style='position:fixed; z-index:999; top:20px; left:200px;'><span style='color:red; font-weight:bold; font-size:16px;'>IE糟糕的兼容性会导致页面显示错误，请换用其他浏览器访问！</span></div>");
-}
 
 // 自适应 电脑/手机 css
-	var isMobile = false;
+	var isMobile = false,
+		ua = navigator.userAgent;
 	if (ua.match(/(windows phone|symbianos|android|mobile|playbook|ipad|iphone|ipod)/i)) {
 		isMobile = true;
 		document.getElementById("css_main").setAttribute("href", "css/mobi_main.css?" + ver);
