67910e65197e32a51a14c30cd6040560b21188a6
LZM <lizheminglzm@gmail.com>
2016-07-16 18:00:36 +0800

    Google Analytics
    取消百度统计，添加Google Analytics

--- js/preload.js
+++ js/preload.js
@@ -16,17 +16,17 @@ else {
  */
 
 if (isOnline){
-	// 百度统计
-	var _hmt = _hmt || [];
-	(function() {
-		var hm = document.createElement("script");
-		hm.src = "//hm.baidu.com/hm.js?af71c0d27742f679093d9e185eee17fb";
-		var s = document.getElementsByTagName("script")[0]; 
-		s.parentNode.insertBefore(hm, s);
-	})();
-	
 	// 腾讯统计
 	document.write("<script type='text/javascript' src='http://tajs.qq.com/stats?sId=40571723' charset='UTF-8'></script>")
+	
+	// Google Analytics
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-46827483-3', 'auto');
+	ga('send', 'pageview');
 }
 
 
